4c48a8059192c63e8ac3fa3fc74f06741b865992
david maravillas <davidmaravillas@pursuit.org>
2020-03-03 19:35:49 -0500

    add proxy to FrontEnd and adjust listening port on router

--- BackEnd/server/app.js
+++ BackEnd/server/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const port = 3000;
+const port = 3001;
 const app = express();
 app.use(cors());
 app.use(bodyParser.urlencoded({extended: false}));
