bbe8ef159d343594c911ced0949cd936df9394ca
david maravillas <davidmaravillas@pursuit.org>
2020-03-03 19:08:58 -0500

    project start

---
+++ BackEnd/server/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const port = 3000;
+const app = express();
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+app.listen(port, () => console.log("Server running on port ", port));
+
