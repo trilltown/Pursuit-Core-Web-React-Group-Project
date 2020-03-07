cf5862c69b3380f3bdf45b184dfee4831d1fc64e
Phillip <phil.awich@gmail.com>
2020-03-07 18:02:23 -0500

    Add username/img to profile page

--- FrontEnd/thebox/src/components/ProfilePage.js
+++ FrontEnd/thebox/src/components/ProfilePage.js
@@ -1,12 +1,38 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useHistory } from 'react-router-dom';
+import '../css/ProfilePage.css'
+import axios from 'axios';
 
 const ProfilePage = () => {
+    const [user, setUser] = useState([]);
 
-    return(
+    useEffect(() => {
+    
+        const fetchData = async () => {
+            try{
+                let res = await axios.get("http://localhost:3001/users/1")
+                // debugger
+                let data = Object.values(res.data.body)
+                setUser(data)
+                console.log(user)
+            } catch(error) {
+                setUser([])
+                console.log(error)
+            }
+        }
+        fetchData()
+    }, [])
+    
+    let userInfo = user.map(info => {
+        debugger
+        return <div> {info.display_name} <img src={info.profile_pic}></img></div>
+    })
+    
+    return( 
+
         <div>
-            ProfilePage
-        </div>
+        {userInfo}
+        </div>      
     )
 }
 
