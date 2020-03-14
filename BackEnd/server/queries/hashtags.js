2dd88e0daa145c092896791e788b15f2cc955d5a
david maravillas <davidmaravillas@pursuit.org>
2020-03-14 11:40:26 -0400

    return full posts by hash query

--- BackEnd/server/queries/hashtags.js
+++ BackEnd/server/queries/hashtags.js
@@ -1,5 +1,28 @@
 const db = require("../db/index")
 
+const postsByHash= async PostList=>{
+    try{
+        let newPost= []
+        
+        while(PostList.length){
+
+                let {search_post_id}=PostList[0]
+                let hash = await db.any("SELECT * FROM posts WHERE id = $1", [search_post_id]) 
+                newPost.push(...hash)
+                PostList.shift()
+    
+        }
+            console.log({newPost})
+        
+        return newPost
+        
+                
+        }catch(error){
+            console.log(error)
+            
+        }
+
+}
 
 const searchHash = async (req, res, next) => {
     console.log(req.query)
@@ -9,11 +32,13 @@ const searchHash = async (req, res, next) => {
             let hash = await db.any("SELECT * FROM hashtags WHERE tag LIKE $1", [`#${hashQuery}`]) 
         
                     if(hash.length){
-            
+
+                        let newList= await postsByHash(hash)
+                        console.log(newList)
                         res.status(200).json({
                             status: "Success",
                             message: "tags found",
-                            body: hash
+                            body: newList
                         })
                     } else{
                             res.status(200).json({
